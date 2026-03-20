95a580aecfd3adce9ddd9bffe53e3b57a71d532a
OelRH <lr16@kent.ac.uk>
2026-03-20 19:06:24 +0000

    created a dealer class which will show the its hand after the player reveals theirs.

---
+++ Dealerclass.py
@@ -0,0 +1,9 @@
+from Playerclass import player
+
+class dealer(player): #The dealer just shows their hand after the player
+    def showhand(self, round_no = 0):
+        if round_no == 1:
+            print("Dealer's hand:", self.hand[1])
+        else:
+            print("Dealer's hand:", self.hand)
+
