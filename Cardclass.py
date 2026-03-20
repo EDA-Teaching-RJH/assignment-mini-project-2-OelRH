7b37023e645c4019c6be4a07063fd532f86dd128
OelRH <lr16@kent.ac.uk>
2026-03-20 18:37:58 +0000

    changed the class of card a bit and started creating the class that defines how the deck works. Also changed number to rank since the face cards are not numbers.

---
+++ Cardclass.py
@@ -0,0 +1,13 @@
+import random
+
+class card:
+    def __init__(self, suit, rank): #This defines the class of the cards
+        self.suit = suit
+        self.number = rank
+
+    def __str__(self):
+        return f"{self.suit} {self.number}" #This returns the objects as a string
+    
+    
+
+
