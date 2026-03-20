691ff7266f173fc46f101860e7e670420538dbe5
OelRH <lr16@kent.ac.uk>
2026-03-20 18:47:45 +0000

    more work on the deck class, able to create objects in deck class and share with the objects in class class

--- Deckclass.py
+++ Deckclass.py
@@ -1,5 +1,13 @@
+import random
 from Cardclass import card
 
 class deck:
-    numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    suits = 
+    ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] #This is a list of cards in a deck
+    suits =  ["diamonds", "hearts", "spades", "clubs"]
+
+    def __init__(self): 
+        self.cards = []
+
+        for rank in deck.ranks: 
+            for suit in deck.suits:
+                self.cards.append(card(suit,rank))
