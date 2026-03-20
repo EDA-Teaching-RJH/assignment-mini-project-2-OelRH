db0c3fd68ae7b361d6e21aaf508e70962e1b1f2c
OelRH <lr16@kent.ac.uk>
2026-03-20 19:01:09 +0000

    created a class for the player which will have hand mechanics and money mechanics for the game.

---
+++ Playerclass.py
@@ -0,0 +1,17 @@
+class player:
+    def __init__ (self):
+        self.hand = []
+        self.cash = 100
+        
+def GetHand(self): 
+    return self.hand
+def GetCard(self, card):
+    self.hand.append(card)
+def ShowHand(self):
+    print(f"Current Hand: {self.hand}") #will show the hand to the user
+def EmptyHand(self):
+    self.hand = []
+def GetCash(self):
+    return self.cash
+
+
