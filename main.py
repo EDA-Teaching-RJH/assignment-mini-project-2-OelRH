fecb7c788f7dc21edb6096461727f025c35de2e9
OelRH <lr16@kent.ac.uk>
2026-03-24 16:19:27 +0000

    starting with betting mechanics

--- main.py
+++ main.py
@@ -17,7 +17,17 @@ def main():
 
         bet = handle_bet(player)
         player.SetCash()
-   
+
+def handle_bet(player):
+    while True:
+        bet = int(input("Please enter amount of cash you wish to bet: £"))
+        if bet > player.GetCash():
+            print("Please bet an amount within your current cash limit. \n")
+        elif bet <= 0:
+            print("please bet a positive amount of money. \n")
+        else:
+            break
+
         player.showhand()
         dealer.showhand(1)
 
